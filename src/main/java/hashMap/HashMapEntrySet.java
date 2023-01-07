78d3d7caee6d13a8f6d5fae77a01376bba378a1f
Aisuluu <aisuluusopubekova88@gmail.com>
2023-01-06 20:02:12 -0600

    i changed something inside hashmapentryset.java class

--- src/main/java/hashMap/HashMapEntrySet.java
+++ src/main/java/hashMap/HashMapEntrySet.java
@@ -1,11 +1,9 @@
 package hashMap;
 
 import java.util.HashMap;
-import java.util.IdentityHashMap;
 import java.util.Map;
 
 public class HashMapEntrySet {
-
 
     public static void main(String[] args) {
         HashMap<Integer, String> numbers = new HashMap<>();
@@ -13,38 +11,25 @@ public class HashMapEntrySet {
         numbers.put(20, "twenty");
         numbers.put(30, "thirty");
         numbers.put(40, "forty");
+
         ////// print only keys
         for (Integer key : numbers.keySet()) {
             System.out.println(key);
         }
+
         ////// print only values
         for (Integer key : numbers.keySet()) {
             System.out.println(numbers.get(key));
-
         }
-
 
         for (Integer key : numbers.keySet()) {
             System.out.println(key + "-" + numbers.get(key));
         }
 
-      for ( Map.Entry<Integer,String> entry: numbers.entrySet()){
-          System.out.println(entry);
+        for (Map.Entry<Integer, String> entry : numbers.entrySet()) {
+            System.out.println(entry);
+        }
 
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("Hello World!");
     }
 }
